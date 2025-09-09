59760fe64df3682388318794ff06b9da5d075412
Kim Hauser <knhauser1s@gmail.com>
2025-09-08 23:58:30 -0500

    tasks completed

--- practice-mixed-type-arrays.js
+++ practice-mixed-type-arrays.js
@@ -1,1 +1,20 @@
+//Task 1:
+
+let userProfileArr = ["Kimi", 32, "NotSubscribed", "USA", "Horse Riding", "Reading", "Gaming"];
+console.log(userProfileArr); 
+
+//Task 2: 
+
+console.log (userProfileArr[0]);
+console.log (userProfileArr[5]);
+
+//Task 3:
+
+userProfileArr[1] = 33;
+userProfileArr[6] = "Sleeping";
+
+//Task 4: 
+
+console.log(userProfileArr); 
+
 
